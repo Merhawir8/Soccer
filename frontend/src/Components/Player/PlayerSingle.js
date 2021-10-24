15437f07e7e6c7f625f8bcd584d3a5be01e87718
Merhawi Huwarshek <Merhawir8@gmail.com>
2021-10-24 12:13:31 -0400

    initial commit

--- frontend/src/Components/Player/PlayerSingle.js
+++ frontend/src/Components/Player/PlayerSingle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PlayerSingle = (props) => {
-    return ( 
+    return (
         <div className="row">
             <div className="col s12 ">
             <div className="card">
@@ -10,15 +10,15 @@ const PlayerSingle = (props) => {
                 <span className="card-title">{props.player.firstName} {props.player.lastName} </span>
                 </div>
                 <div className="card-content">
-                <p>Phone : {props.player.phone} - Email: {props.player.email}</p>
-                <p>Strength: {props.player.strength} - Endurance: {props.player.endurance}</p>
+                    <p>Phone : {props.player.phone} - Email: {props.player.email}</p>
+                    <p>Strength: {props.player.strength} - Endurance: {props.player.endurance}</p>
                 </div>
                 <div className="card-action">
                     Team: {props.player.team}
                 </div>
             </div>
             </div>
-        </div
+        </div>
     );
 }
 
