b2b20891249086a7b873b4fb356e3e9c9beea309
Merhawi Huwarshek <Merhawir8@gmail.com>
2021-10-26 12:39:31 -0400

    initial commit

--- frontend/src/Components/Player/PlayerForm.js
+++ frontend/src/Components/Player/PlayerForm.js
@@ -6,10 +6,37 @@ class PlayerForm extends React.Component {
         this.state = {  }
     }
     render() { 
-        return ( <div>
-            PlayerForm
-        </div> );
+        return ( 
+            <div class="row">
+                <h1 className="center">Add a new player</h1>
+                <form class="col s12" onSubmit={this.submitPlayer.bind(this)}>
+                    <div class="row">
+                        <div class="input-field col s6">
+                        <input  id="firstName" ref="firstName" type="text"  />
+                        <label htmlFor="firstName">First Name</label>
+                        </div>
+                        <div class="input-field col s6">
+                        <input  id="lastName" ref="lastName" type="text"  />
+                        <label htmlFor="lastName">Last Name</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s6">
+                        <input  id="phone" ref="phone" type="text"  />
+                        <label htmlFor="phone">Phone</label>
+                        </div>
+                        <div class="input-field col s6">
+                        <input  id="email" ref="email" type="text"  />
+                        <label htmlFor="email">Email</label>
+                        </div>
+                    </div>
+                    <button className="btn waves-effect waves-light" type="submit"
+                    name="action">Add player</button>
+                </form>
+            </div>
+        );
     }
 }
- 
+
 export default PlayerForm;
